2d2061b76f727914d33d8c192c88b90d9251d0f9
realroy <roiboon.cc@gmail.com>
2023-08-03 23:02:02 +0700

    cosmetic: front page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,40 @@
+import Link from "next/link";
+
+import {
+  Card,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/card";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-center font-bold">Black Helmet</h1>
+    <main className="min-h-screen max-w-5xl mx-auto">
+      <div className="flex items-center justify-between pt-9">
+        <h1 className="text-center font-bold text-3xl">Black Helmet</h1>
+      </div>
+      <ul className="grid grid-cols-2 py-4 gap-4">
+        <Link href={"/business-documents"}>
+          <li>
+            <Card>
+              <CardHeader>
+                <CardTitle>Business Documents</CardTitle>
+                <CardDescription>
+                  Manage your business documents
+                </CardDescription>
+              </CardHeader>
+            </Card>
+          </li>
+        </Link>
+        <li>
+          <Card>
+            <CardHeader>
+              <CardTitle>Coming soon</CardTitle>
+              <CardDescription>TBA</CardDescription>
+            </CardHeader>
+          </Card>
+        </li>
+      </ul>
     </main>
   );
 }
