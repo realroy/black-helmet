e9f365e55cddbed29509b130deec0b26f313df7c
realroy <roiboon.cc@gmail.com>
2023-09-16 23:50:09 +0700

    fix(components): remove unused

--- app/(app)/business-documents/_components/index.ts
+++ app/(app)/business-documents/_components/index.ts
@@ -1,6 +1,5 @@
 export * from "./business-document";
 export * from "./form-business-document";
 export * from "./header-business-document";
-export * from "./printable-business-document";
 export * from "./table-business-document-products";
 export * from "./table-business-documents";
