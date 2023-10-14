5f619f75a947cc34e15b268d504cb54d0470f70d
realroy <roiboon.cc@gmail.com>
2023-10-14 09:17:03 +0700

    feat(service): add resolve status logic

--- services/create-relevant-business-document-by-user.ts
+++ services/create-relevant-business-document-by-user.ts
@@ -1,3 +1,5 @@
+import { eq } from "drizzle-orm";
+
 import { businessDocuments, db, relevantBusinessDocuments } from "@/db";
 
 import type { BusinessDocument, User } from "@/types";
@@ -23,6 +25,13 @@ export async function createRelevantBusinessDocumentByUser(
 
   let output: BusinessDocument | undefined;
 
+  const { sourceBusinessDocumentNewStatus, targetBusinessDocumentStatus } =
+    resolveBusinessDocumentStatuses({
+      sourceBusinessDocumentKind: input.businessDocument.kind,
+      sourceBusinessDocumentStatus: input.businessDocument.status,
+      targetBusinessDocumentKind: input.targetKind,
+    });
+
   await db.transaction(async (tx) => {
     const insertResults = await tx
       .insert(businessDocuments)
@@ -30,11 +39,19 @@ export async function createRelevantBusinessDocumentByUser(
         ...sourceBusinessDocumentData,
         documentNo: input.targetDocumentNo,
         issueDate: new Date(),
+        status: targetBusinessDocumentStatus,
         kind: input.targetKind,
       })
       .returning();
 
     output = insertResults[0];
+
+    await tx
+      .update(businessDocuments)
+      .set({
+        status: sourceBusinessDocumentNewStatus,
+      })
+      .where(eq(businessDocuments.id, input.businessDocument.id));
 
     await tx.insert(relevantBusinessDocuments).values({
       userId: input.userId,
@@ -50,4 +67,45 @@ export async function createRelevantBusinessDocumentByUser(
   });
 
   return output;
+}
+
+function resolveBusinessDocumentStatuses(input: {
+  sourceBusinessDocumentKind: BusinessDocument["kind"];
+  sourceBusinessDocumentStatus: BusinessDocument["status"];
+  targetBusinessDocumentKind: BusinessDocument["kind"];
+}): {
+  sourceBusinessDocumentNewStatus: BusinessDocument["status"];
+  targetBusinessDocumentStatus: BusinessDocument["status"];
+} {
+  const {
+    sourceBusinessDocumentKind,
+    sourceBusinessDocumentStatus,
+    targetBusinessDocumentKind,
+  } = input;
+
+  if (
+    sourceBusinessDocumentKind === "QUOTATION" &&
+    sourceBusinessDocumentStatus === "PENDING" &&
+    targetBusinessDocumentKind === "INVOICE"
+  ) {
+    return {
+      sourceBusinessDocumentNewStatus: "ISSUED",
+      targetBusinessDocumentStatus: "PENDING",
+    };
+  }
+
+  if (
+    sourceBusinessDocumentKind === "INVOICE" &&
+    sourceBusinessDocumentStatus === "PENDING" &&
+    targetBusinessDocumentKind === "RECEIPT"
+  ) {
+    return {
+      sourceBusinessDocumentNewStatus: "RECEIPT_CREATED",
+      targetBusinessDocumentStatus: "PENDING",
+    };
+  }
+
+  throw new Error(
+    `Cannot resolve source business document status from source.kind = ${sourceBusinessDocumentKind} and source.status = ${sourceBusinessDocumentStatus} to target.kind ${targetBusinessDocumentKind}`
+  );
 }
