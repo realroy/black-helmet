30bf4973acadd1cf28f151a2fb5f495ca84ca2b1
realroy <roiboon.cc@gmail.com>
2023-10-14 08:25:00 +0700

    feat(service): change logic

--- services/create-relevant-business-document-by-user.ts
+++ services/create-relevant-business-document-by-user.ts
@@ -4,37 +4,50 @@ import type { BusinessDocument, User } from "@/types";
 
 export type CreateRelevantBusinessDocumentByUserInput = {
   businessDocument: BusinessDocument;
+  targetDocumentNo: BusinessDocument["documentNo"];
+  targetKind: BusinessDocument["kind"];
   userId: User["id"];
 };
 
 export async function createRelevantBusinessDocumentByUser(
   input: CreateRelevantBusinessDocumentByUserInput
 ) {
-  const { id, createdAt, updatedAt, issueDate, ...sourceBusinessDocumentData } =
-    input.businessDocument;
+  const {
+    id,
+    createdAt,
+    updatedAt,
+    issueDate,
+    dueDate,
+    ...sourceBusinessDocumentData
+  } = input.businessDocument;
 
-  let targetBusinessDocument: BusinessDocument | undefined;
+  let output: BusinessDocument | undefined;
 
   await db.transaction(async (tx) => {
     const insertResults = await tx
       .insert(businessDocuments)
-      .values(sourceBusinessDocumentData)
+      .values({
+        ...sourceBusinessDocumentData,
+        documentNo: input.targetDocumentNo,
+        issueDate: new Date(),
+        kind: input.targetKind,
+      })
       .returning();
 
-    targetBusinessDocument = insertResults[0];
+    output = insertResults[0];
 
     await tx.insert(relevantBusinessDocuments).values({
       userId: input.userId,
       sourceBusinessDocumentId: input.businessDocument.id,
-      targetBusinessDocumentId: targetBusinessDocument.id,
+      targetBusinessDocumentId: output.id,
     });
 
     await tx.insert(relevantBusinessDocuments).values({
       userId: input.userId,
-      sourceBusinessDocumentId: targetBusinessDocument.id,
+      sourceBusinessDocumentId: output.id,
       targetBusinessDocumentId: input.businessDocument.id,
     });
   });
 
-  return targetBusinessDocument;
+  return output;
 }
