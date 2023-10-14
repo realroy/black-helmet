ead12463a1b9082da528e54b01cf59138a13a613
realroy <roiboon.cc@gmail.com>
2023-10-14 07:25:53 +0700

    refactor(db): drop quotation table

--- db/schema.ts
+++ db/schema.ts
@@ -11,11 +11,7 @@ import {
   varchar,
 } from "drizzle-orm/pg-core";
 
-import type {
-  BusinessDocumentKind,
-  BusinessDocumentProduct,
-  QuotationProduct,
-} from "@/types";
+import type { BusinessDocumentKind, BusinessDocumentProduct } from "@/types";
 
 const defaultColumns = {
   id: serial("id").primaryKey(),
@@ -26,46 +22,6 @@ const defaultColumns = {
     .notNull()
     .defaultNow(),
 };
-
-export const quotation = pgTable(
-  "quotations",
-  {
-    ...defaultColumns,
-    documentNo: varchar("document_no", { length: 100 }).notNull(),
-    customerName: varchar("customer_name").notNull(),
-    customerAddress: text("customer_address").notNull(),
-    customerZipCode: varchar("customer_zip_code").notNull(),
-    customerTaxId: varchar("customer_tax_id").notNull(),
-    customerBranch: varchar("customer_branch").notNull(),
-    issueDate: timestamp("issue_date", { withTimezone: true })
-      .notNull()
-      .defaultNow(),
-    dueDate: timestamp("due_date", { withTimezone: true })
-      .notNull()
-      .defaultNow(),
-    projectName: varchar("project_name").notNull(),
-    products: jsonb("products").$type<QuotationProduct[]>().default([]),
-    subTotal: decimal("sub_total", { precision: 100, scale: 2 })
-      .notNull()
-      .default("0.00"),
-    grandTotal: decimal("grand_total", { precision: 100, scale: 2 })
-      .notNull()
-      .default("0.00"),
-    withholdingTax: decimal("withholding_tax", { precision: 100, scale: 2 })
-      .notNull()
-      .default("0.00"),
-    paymentAmount: decimal("payment_amount", { precision: 100, scale: 2 })
-      .notNull()
-      .default("0.00"),
-    sellerName: varchar("seller_name").notNull(),
-    userId: integer("user_id")
-      .notNull()
-      .references(() => user.id),
-  },
-  (quotations) => ({
-    userIndex: index("quotations_user_id_idx").on(quotations.userId),
-  })
-);
 
 export const user = pgTable(
   "users",
--- types/index.ts
+++ types/index.ts
@@ -1,28 +1,3 @@
 export * from "./auth";
-
-import { InferModel } from "drizzle-orm";
-
-import { z } from "zod";
-
-import {
-  createBusinessDocumentSchema,
-  createQuotationSchema,
-  updateBusinessDocumentSchema,
-  updateQuotationSchema,
-} from "@/schemas";
-import { businessDocuments, quotation, user } from "@/db";
-
-export * from "./auth";
 export * from "./business-document";
 export * from "./common";
-
-export type Quotation = typeof quotation.$inferInsert;
-export type QuotationProduct = {
-  name: string;
-  quantity: number;
-  unit: string;
-  unitPrice: number;
-};
-
-export type CreateQuotation = z.infer<typeof createQuotationSchema>;
-export type UpdateQuotation = z.infer<typeof updateQuotationSchema>;
